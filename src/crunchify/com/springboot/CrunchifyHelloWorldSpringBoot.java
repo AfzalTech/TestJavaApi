ae3c879940e16ad81153f7b35e580038c60c99a8
afzalvirani <afzalvirani@hotmail.com>
2019-12-18 12:26:16 +0530

    ok

---
+++ src/crunchify/com/springboot/CrunchifyHelloWorldSpringBoot.java
@@ -0,0 +1,31 @@
+package crunchify.com.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class CrunchifyHelloWorldSpringBoot {
+
+	@RequestMapping("/")
+	String basicURL() {
+	
+		return "Welcome to Spring Boot Hello World Tutorial";
+	
+	}
+ 
+	@RequestMapping("crunchify")
+	String crunchifyURL() {
+		
+		return "Hello Crunchify Friends! This is your first SpringBoot Example. Isn't that so Simple?";
+	
+	}
+ 
+	public static void main(String[] args) throws Exception {
+ 
+		// SpringApplication classes that can be used to bootstrap and launch a Spring application from a Java
+		// main method. By default class will perform the following steps to bootstrap your application.
+		SpringApplication.run(CrunchifyHelloWorldSpringBoot.class, args);
+	}
+}
